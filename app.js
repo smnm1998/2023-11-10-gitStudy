ace747bd937143b164592949f0013a09806ee441
smnm1998 <dltpals7498@gmail.com>
2023-11-10 16:11:39 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/main', function (req, res) {
+  res.send('main 페이지입니다.');
+});
+
+app.get('/test', function (req, res) {
+  res.send('test 페이지입니다.');
+});
+
+const PORT = 8080;
+app.listen(PORT, function () {
+  console.log('Listening on port: ', PORT);
+});
